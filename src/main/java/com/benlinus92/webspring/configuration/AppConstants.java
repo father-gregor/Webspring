ac34e9f3d8c2dad6c9076ad68a4c37bc8b43d10f
father-gregor <benlinus92@gmail.com>
2015-11-27 19:21:09 +0200

    New file for constants

---
+++ src/main/java/com/benlinus92/webspring/configuration/AppConstants.java
@@ -0,0 +1,7 @@
+package com.benlinus92.webspring.configuration;
+
+public final class AppConstants {
+	public static final String CURR_API_HISTORY = "http://apilayer.net/api/historical?access_key=194a276698ccbe38f3d622a013f4dc64&date=";
+	public static final String CURR_API_SET = "&currencies="; 
+	public static final String CURR_API_RATESLIST = "CAD,UAH,RUB,GBP,EUR,AED";
+}
